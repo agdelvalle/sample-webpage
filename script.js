e92e21fe6452596c24497a25fb1322c16323118c
unknown <aly.delvalle.9@gmail.com>
2022-02-10 00:21:45 +0800

    switch to owl-carousel

--- script.js
+++ script.js
@@ -2,63 +2,63 @@
 
 // image slider
 
-let slider = tns({
-    container: '.my-slider',
-    "slideBy": '1',
-    "speed": 400,
-    "nav": false,
-    autowidth: true,
-    loop: true,
-    autoplay: false,
-    controls: true,
-    autoplayButtonOutput: true,
-    responsive: {
-        1000: {
-            items: 3
+let $owl = $('.owl-carousel').owlCarousel({
+    loop: false,
+    rewind: true,
+    margin:0,
+    center: false,
+    nav:true,
+    responsive:{
+        0:{
+            items: 1,
         },
-        800: {
-            items: 1
+        750:{
+            items:3
         }
     }
 })
-
-// $(document).ready(function(){
-//     $(".owl-carousel").owlCarousel();
-//   });
-
 // fetch images for slider
 let loadImgBtn = document.getElementById("loadImgBtn");
-let getSlider = document.querySelector(".my-slider");
+// let getSlider = document.querySelector(".my-slider");
+let getSlider = document.querySelector("#owl-demo");
 let slideOne = document.getElementById("slide-one");
 let slideTwo = document.getElementById("slide-two");
 let slideThree = document.getElementById("slide-three");
-// let slideFour = document.getElementById("slide-four");
 
-function getFirstThreeImages(){
+// let randomCatAge = Math.floor((Math.random() * 9) + 1);
+
+(function getFirstThreeImages(){
     console.log("click");
     fetch('https://api.thecatapi.com/v1/images/search', 
         {headers: {
             'x-api-key':'863d2870-6544-4ed7-a15a-c024729f17ed', 
         }})
         .then((res) => res.json())
-        .then((data) => slideOne.innerHTML = `<img src=${data[0].url}>`)
+        .then((data) => {
+            urlOne = data[0].url;
+            slideOne.innerHTML = `<img src=${data[0].url}><p>Age:${Math.floor((Math.random() * 9) + 1)}</p>`
+        })
     
     fetch('https://api.thecatapi.com/v1/images/search', 
         {headers: {
             'x-api-key':'863d2870-6544-4ed7-a15a-c024729f17ed', 
         }})
         .then((res) => res.json())
-        .then((data) => slideTwo.innerHTML = `<img src=${data[0].url}>`)
+        .then((data) => {
+            urlTwo = data[0].url;
+            slideTwo.innerHTML = `<img src=${data[0].url}><p>Age:${Math.floor((Math.random() * 9) + 1)}</p>`
+        })
 
     fetch('https://api.thecatapi.com/v1/images/search', 
         {headers: {
             'x-api-key':'863d2870-6544-4ed7-a15a-c024729f17ed', 
         }})
         .then((res) => res.json())
-        .then((data) => slideThree.innerHTML = `<img src=${data[0].url}>`)
-};
-
-getFirstThreeImages();
+        .then((data) => {
+            urlThree = data[0].url;
+            slideThree.innerHTML = `<img src=${data[0].url}><p>Age:${Math.floor((Math.random() * 9) + 1)}</p>`
+        })
+})();
 
 const getNewImage = () => {
     console.log("click");
@@ -68,10 +68,10 @@ const getNewImage = () => {
         }})
         .then((res) => res.json())
         .then((data) => {
-            slider.destroy();
-            getFirstThreeImages();
-            getSlider.insertAdjacentHTML("afterend", `<div class="slide"><img src=${data[0].url}></div>`);
-            slider.rebuild();
+            // $('.owl-carousel')
+            $owl
+            .owlCarousel('add', `<div class="slide"><img src=${data[0].url}><p>Age:${Math.floor((Math.random() * 9) + 1)}</p></div>`)
+            .owlCarousel('update')
         })
     }
 
